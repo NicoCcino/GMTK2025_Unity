51978799162ed61d88da4c5fdf36fe9508013552
LiliLaTipuce <olivia_1988@hotmail.fr>
2025-08-02 10:42:27 +0200

    Main Menu scene

---
+++ Assets/Core/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    // Method to start the game by loading the first scene
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scene_Freepouille");
+    }
+
+    // Method to quit the application
+   /* public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is quitting...");
+    }*/
+}
